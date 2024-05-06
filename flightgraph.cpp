a785af813121513720595f5c2eab982fc371b7ce
harshalmhetre <150924017+harshalmhetre@users.noreply.github.com>
2024-05-06 07:53:17 +0530

    Update flightgraph.cpp

--- flightgraph.cpp
+++ flightgraph.cpp
@@ -1,79 +1,90 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
 
-class flight{
-    int n;
-    string city[10];
-    int a[10][10];
-public:
-    void DFS(int v, bool visited[]) {
-        visited[v] = true;
-        for (int i = 0; i < n; ++i) {
-            if (a[v][i]!=0 && !visited[i]) {
-                DFS(i, visited);
-            }
-        }
-    }
-    
-    void getdata() {
-        cout<<"Enter the number of cities:";
-        cin>>n;
-        cout<<"Enter the names of cities:"<<endl;
-        for(int i = 1; i <= n; i++) {
-            cout<<"City "<<i <<": ";
-            cin>>city[i];
-        }
-        cout<<"Adjacency matrix representing flight paths (1 for flight exists, 0 for no flight):"<<endl;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                cout<<"Flight between "<<city[i]<<"&"<<city[j]<<"is present:";
-                cin>>a[i][j];
-                if(a[i][j] == 1) {
-                    cout<<"Enter the time required for flight to travel the path in min: ";
-                    cin>>a[i][j];
-                }
-            }
-        }
-    }
-    
-    void display() {
-        cout<<"\nAdjacency matrix: "<<endl;
-        cout<<"  ";
-        for(int i = 0; i < n; ++i) {
-            cout<<city[i]<<" ";
-        }
-        cout<<endl;
-        for(int i = 0; i < n; ++i) {
-            cout<<city[i]<<" ";
-            for(int j = 0; j < n; ++j) {
-                cout<<a[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-    }
 
-    bool isconnect() {
-        bool visited[n] = {false};
-        
-        DFS(0, visited); 
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i]) {
-                return false; 
-            }
-        }
-        return true;
-    }
-};
 
 int main() {
-    flight g;
-    g.getdata();
-    g.display();
-    if(g.isconnect()) {
-        cout<<"The flight path is connected";
-    }
-    else {
-        cout<<"The flight path is not connected";
-    }
+    
+     int n,t;
+     string city[10];
+     char ch;
+     cout<<"\nenter number of cities:";
+     cin>>n;
+     cout<<"\nenter names of cities:";
+     for(int i=0;i<n;i++)
+     {
+         cin>>city[i];
+     }
+     int time1[10][10];
+     
+     for(int i=0;i<n;i++)
+     {
+         for(int j=0;j<n;j++)
+         {
+             time1[i][j]=0;
+         }
+     }
+     
+     cout<<"\nenter paths:";
+     for(int i=0;i<n;i++)
+     {
+         
+         for(int j=0;j<n;j++)
+         {
+             if(i!=j && time1[j][i]==0)
+             {
+             cout<<"\nis there path from "<<city[i]<<" to "<<city[j]<<":";
+             cin>>ch;
+             if(ch=='y')
+             {
+                 cout<<"\nenter time required to reach "<<city[i]<<" to "<<city[j]<<":";
+                 cin>>t;
+                 time1[i][j]=t;
+                 time1[j][i]=t;
+             }
+             }
+         }
+     }
+     
+     cout<<"the adjancency matrix representation is:\n";
+     for(int i=0;i<n;i++)
+     {
+         cout<<"\t"<<city[i];
+     }
+     cout<<"\n";
+     for(int i=0;i<n;i++)
+     {
+         cout<<city[i]<<"\t";
+         for(int j=0;j<n;j++)
+         {
+             cout<<time1[i][j]<<"\t";
+         }
+         cout<<"\n";
+     }
+     
+     int c=0;
+     for(int i=0;i<n;i++)
+     {
+         for(int j=0;j<n;j++)
+         {
+             if(time1[i][j]==0)
+             {
+                c++;
+             }
+         }
+         if(c==n)
+         {
+             break;
+         }
+     }
+     if(c==n)
+     {
+         cout<<"graph is not connected";
+     }
+     else
+     {
+         cout<<"graph is connected";
+     }
     return 0;
 }
